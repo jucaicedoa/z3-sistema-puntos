1d86f409438251d096f982e578f4d4cd85974e87
jucaicedoa <jucaicedoa@unal.edu.co>
2026-04-12 20:43:46 -0500

    fix: ajuste precios

--- src/app/precios/precios.component.ts
+++ src/app/precios/precios.component.ts
@@ -15,12 +15,12 @@ export interface ComboRow {
 })
 export class PreciosComponent {
   readonly combos: ComboRow[] = [
-    { item: 'COMBO BUÑUELOS (5 BUÑUELOS)', price: '$5.000' },
-    { item: 'COMBO BUÑUELOS (5 BUÑUELOS) + CAFÉ/TINTO', price: '$5.500' },
-    { item: 'COMBO BUÑUELOS RELLENOS (4 BUÑUELOS)', price: '$5.000' },
-    { item: 'COMBO BUÑUELOS RELLENOS (4 BUÑUELOS Y 1 CAFÉ/TINTO)', price: '$5.500' },
-    { item: 'COMBO EMPANADAS (3 EMPANADAS)', price: '$9.000' },
-    { item: 'COMBO EMPANADAS (3 EMPANADAS + GASEOSA)', price: '$9.500' },
+    { item: 'COMBO BUÑUELOS 1 (5 BUÑUELOS)', price: '$5.000' },
+    { item: 'COMBO BUÑUELOS 2 (5 BUÑUELOS + CAFÉ/TINTO)', price: '$5.500' },
+    { item: 'COMBO BUÑUELOS RELLENOS 1 (4 BUÑUELOS RELLENOS)', price: '$5.000' },
+    { item: 'COMBO BUÑUELOS RELLENOS 2 (4 BUÑUELOS Y 1 CAFÉ/TINTO)', price: '$5.500' },
+    { item: 'COMBO EMPANADAS 1 (3 EMPANADAS)', price: '$9.000' },
+    { item: 'COMBO EMPANADAS 2 (3 EMPANADAS + VASO DE GASEOSA)', price: '$9.500' },
     { item: 'CHORIZO (PAPA, LIMÓN, CHORIZO, TOPPING)', price: '$9.000' },
     { item: 'CHORIZO COMBO (2 CHORIZOS + 2 ACOMPAÑAMIENTOS: PAPA/AREPA/ENSALADA, 2 VASOS DE GASEOSA)', price: '$16.000' },
     { item: 'CHUZO POLLO (PAPA,LIMÓN, CHUZO POLLO, TOPPING)', price: '$13.000' },
